122c7ea9fe49ffa3e8aad0c0b0c7aa35d26587cb
Joep de Jong <j.dejong-16@student.tudelft.nl>
2021-09-29 17:39:12 +0200

    Update timeline

--- web/src/components/Timeline/index.jsx
+++ web/src/components/Timeline/index.jsx
@@ -9,36 +9,82 @@ const Timeline = () => {
             <div data-num="6" className="Activity">
                 <h3>Pre lustrum: Canoeing Thursday</h3>
                 <p>During this activity we will be canoeing in the Abtswoudse Bos in Delft!</p>
-                <a href="https://wisv.ch" tbuttonrget="_blank" rel="noreferrer" className="Button Button--disabled">Sold out</a>
+                <a href="https://ch.tudelft.nl/media/photos/" tbuttonrget="_blank" rel="noreferrer" className="Button">Photos</a>
             </div>
             <div data-num="7" className="Activity">
                 <h3>Pre lustrum: Canoeing Friday</h3>
                 <p>During this activity we will be canoeing in the Abtswoudse Bos in Delft!</p>
-                <a href="https://wisv.ch" target="_blank" rel="noreferrer" className="Button  Button--disabled" disabled>Sold out</a>
+                <a href="https://ch.tudelft.nl/media/photos/" target="_blank" rel="noreferrer" className="Button">Photos</a>
             </div>
             <div className="Title"><h4>Sep</h4></div>
             <div data-num="15" className="Activity">
-                <h3>Day at the BeaCH</h3>
-                <p>At our first activity in the 65th year we will enjoy the weather on a day at the beach. Together with your fellow CH-members you can play beach games. Afterwards, there is a possibility to have dinner.</p>
-                <a href="https://instagram.com/lucie_wisvch" target="_blank" rel="noreferrer" className="Button">Stay posted</a>
+                <h3>Beachday</h3>
+                <p>Buy a beach ticket for €2 to enjoy some drinks, snacks, and various beach games with us from 15:00 until 18:00.</p>
+                <p>If you want to eat dinner, you can buy a ticket for €17,50 which includes a ticket to the beach. The dinner starts on 18:00 at beachclub Whoosah. Fill in the form to select a preference of pizza, a burger or chicken sate.</p>
+                {/* <a href="https://ch.tudelft.nl/events/f44ffc82-5426-4bbd-ac89-c91ef874df80" target="_blank" rel="noreferrer" className="Button">Tickets</a> */}
+                {/* <a href="https://forms.gle/Bsx7fy4k6N2ThFpz8" target="_blank" rel="noreferrer" className="Button">Dinner form</a> */}
             </div>
             <div className="Title"><h4>Nov</h4></div>
-            <div data-num="15" className="Activity">
-                <h3>Week 1: Lustrum opening</h3>
-                <p>The kick-off of our first lustrum..</p>
-                <a href="https://instagram.com/lucie_wisvch" target="_blank" rel="noreferrer" className="Button">Stay posted</a>
-            </div>
             <div className="Day">
-                <div data-num="16" className="Activity">
-                    <h3>Week 1: Lustrum auction</h3>
-                    <p>...</p>
+                <div data-num="15" className="Activity">
+                    <h3>Week 1: Lustrum opening</h3>
+                    <p>On Monday we will kick off our first lustrum week with a grand opening show held on campus. Come join us at this owlsome activity!</p>
                     <a href="https://instagram.com/lucie_wisvch" target="_blank" rel="noreferrer" className="Button">Stay posted</a>
                 </div>
                 <div className="Activity">
-                    <h3>Week 1: Lustrum lecture</h3>
-                    <p>The activities for the first lustrum week are not announced yet.. Follow us on Instagram to stay posted about all our activities!</p>
+                    <h3>Week 1: Lustrum reception</h3>
+                    <p>After the grand opening show, a reception will be held for honorary members and members of merit. This reception will take place at the /Pub and is on invite only.</p>
                     <a href="https://instagram.com/lucie_wisvch" target="_blank" rel="noreferrer" className="Button">Stay posted</a>
                 </div>
+                <div className="Activity">
+                    <h3>Week 1: Opening party</h3>
+                    <p>Of course our first lustrum week is opened with an opening party. There is no better location for this than the beloved STECK. The opening party will be a themed party, so follow us on Instagram to make sure you do not miss the theme announcement!</p>
+                    <a href="https://instagram.com/lucie_wisvch" target="_blank" rel="noreferrer" className="Button">Stay posted</a>
+                </div>
+            </div>
+            <div className="Day">
+                <div data-num="16" className="Activity">
+                    <h3>Week 1: Lecture Maartje Laterveer</h3>
+                    <p>Maartje Laterveer will give a lecture in the first lustrum week. She writes about diversity and inclusion in a fast changing world of business and politics, for het Financieele Dagblad, de Volkskrant and Vogue Netherlands. She is specialized in essays and columns, interviews human interest and profiles. She is going to talk about feminism and women in the business world.</p>
+                    <a href="https://instagram.com/lucie_wisvch" target="_blank" rel="noreferrer" className="Button">Stay posted</a>
+                </div>
+                <div className="Activity">
+                    <h3>Week 1: Auction</h3>
+                    <p>In week 1 we also will give a nice lustrum Auction. During this evening the lustrum committee will auction different objects ranging from mathematics literature to helpful ‘stuko’ packages. Besides the auction we will drink a beer together in the /Pub. Make sure to attend this event to be able to buy some amazing stuff for student prices!</p>
+                    <a href="https://instagram.com/lucie_wisvch" target="_blank" rel="noreferrer" className="Button">Stay posted</a>
+                </div>
+            </div>
+            <div className="Day">
+                <div data-num="17" className="Activity">
+                    <h3>Week 1: Workshop at Da Vinci Derivatives</h3>
+                    <p>Learn how to apply the quantitative skills that you have developed during your studies in the world of trading. During this workshop Da Vinci Derivatives will teach you about the ins and outs of trading during interactive trading games, an office tour and a Q & A with employees that have recently graduated from their studies.</p>
+                    <a href="https://instagram.com/lucie_wisvch" target="_blank" rel="noreferrer" className="Button">Stay posted</a>
+                </div>
+                <div className="Activity">
+                    <h3>Week 1: Comedy Night</h3>
+                    <p>During the comedy night laughing is guaranteed. Comedian Mino van Nassau will take care of this. He is a comedian with an Indian flavour. His Indian background plays a major role in his stand-up comedy.</p>
+                    <a href="https://instagram.com/lucie_wisvch" target="_blank" rel="noreferrer" className="Button">Stay posted</a>
+                </div>
+            </div>
+            <div data-num="18" className="Activity">
+                <h3>Week 1: Beer tasting</h3>
+                <p>Who has the best taste? On thursday we will gather at Het Proeflokaal (tasting room) of the Delfts brewery De Koperen Kat. In a timespan of 2 hours, several beers will be served. We’d love to see you there!</p>
+                <a href="https://instagram.com/lucie_wisvch" target="_blank" rel="noreferrer" className="Button">Stay posted</a>
+            </div>
+            <div data-num="19" className="Activity">
+                <h3>Week 1: "Uitbrak" high tea</h3>
+                <p>As a closer for the first lustrum week we will be having a nice high tea in the /Pub during the lunch break. Come on down to have some delicious traditional scones along with many other different sweet or savory bites!</p>
+                <a href="https://instagram.com/lucie_wisvch" target="_blank" rel="noreferrer" className="Button">Stay posted</a>
+            </div>
+            <div data-num="27" className="Activity">
+                <h3>CHess tournament</h3>
+                <p>On the 27th of November a CHess tournament will be held. This activity is open for all TU Students. The location will be announced soon!</p>
+                <a href="https://instagram.com/lucie_wisvch" target="_blank" rel="noreferrer" className="Button">Stay posted</a>
+            </div>
+            <div className="Title"><h4>Dec</h4></div>
+            <div data-num="22" className="Activity">
+                <h3>Pub marathon</h3>
+                <p>Have you missed the /Pub during corona as much as we have? Now is your time to catch up! Come join us for a full 13 hours in the /Pub filled with entertainment and activities. Lunch, dinner and the neccesary refreshments are provided.</p>
             </div>
             <div className="Title"><h4>2022</h4></div>
             <div className="Title"><h4>Mar</h4></div>
