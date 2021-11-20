ad2de5ac2302c26e496af8b313deee4ddbbbec71
Joep de Jong <j.dejong-16@student.tudelft.nl>
2021-11-20 15:41:07 +0100

    Update timeline

--- web/src/components/Timeline/index.jsx
+++ web/src/components/Timeline/index.jsx
@@ -34,47 +34,47 @@ const Timeline = () => {
                 <div className="Activity">
                     <h3>Week 1: Lustrum reception</h3>
                     <p>After the grand opening show, a reception will be held for honorary members and members of merit. This reception will take place at the /Pub and is on invite only.</p>
-                    <a href="https://instagram.com/lucie_wisvch" target="_blank" rel="noreferrer" className="Button Button--disabled">Invitation only</a>
+                    <button target="_blank" rel="noreferrer" className="Button Button--disabled">Invitation only</button>
                 </div>
                 <div className="Activity">
                     <h3>Week 1: Opening party</h3>
                     <p>Of course our first lustrum week is opened with an opening party. There is no better location for this than the beloved STECK. The opening party will be a themed party, so follow us on Instagram to make sure you do not miss the theme announcement!</p>
-                    <a href="https://ch.tudelft.nl/events/4bf7664c-8484-42ff-9113-6272ee187280" target="_blank" rel="noreferrer" className="Button">Tickets</a>
+                    <button className="Button Button--danger" disabled>Cancelled</button>
                 </div>
             </div>
             <div className="Day">
                 <div data-num="16" className="Activity">
                     <h3>Week 1: Lecture Maartje Laterveer</h3>
                     <p>Maartje Laterveer will give a lecture in the first lustrum week. She writes about diversity and inclusion in a fast changing world of business and politics, for het Financieele Dagblad, de Volkskrant and Vogue Netherlands. She is specialized in essays and columns, interviews human interest and profiles. She is going to talk about feminism and women in the business world.</p>
-                    <a href="https://ch.tudelft.nl/events/46dd4442-caee-48c3-b42a-0768515663d3" target="_blank" rel="noreferrer" className="Button">Tickets</a>
+                    {/* <a href="https://ch.tudelft.nl/events/46dd4442-caee-48c3-b42a-0768515663d3" target="_blank" rel="noreferrer" className="Button">Tickets</a> */}
                 </div>
                 <div className="Activity">
                     <h3>Week 1: Auction</h3>
                     <p>In week 1 we also will give a nice lustrum Auction. During this evening the lustrum committee will auction different objects ranging from mathematics literature to helpful ‘stuko’ packages. Besides the auction we will drink a beer together in the /Pub. Make sure to attend this event to be able to buy some amazing stuff for student prices!</p>
-                    <a href="https://ch.tudelft.nl/events/6ef3439e-7bed-4854-b90d-e21840c81c41" target="_blank" rel="noreferrer" className="Button">Tickets</a>
+                    {/* <a href="https://ch.tudelft.nl/events/6ef3439e-7bed-4854-b90d-e21840c81c41" target="_blank" rel="noreferrer" className="Button">Tickets</a> */}
                 </div>
             </div>
             <div className="Day">
                 <div data-num="17" className="Activity">
                     <h3>Week 1: Workshop at Da Vinci Derivatives</h3>
                     <p>Learn how to apply the quantitative skills that you have developed during your studies in the world of trading. During this workshop Da Vinci Derivatives will teach you about the ins and outs of trading during interactive trading games, an office tour and a Q &amp; A with employees that have recently graduated from their studies.</p>
-                    <a href="https://forms.gle/fggCxVzncSCTghRP6" target="_blank" rel="noreferrer" className="Button">Interest form</a>
+                    <button href="/" target="_blank" rel="noreferrer" className="Button Button--disabled" disabled>Registration closed</button>
                 </div>
                 <div className="Activity">
                     <h3>Week 1: Comedy Night</h3>
                     <p>During the comedy night laughing is guaranteed. Comedian Mino van Nassau will take care of this. He is a comedian with an Indian flavour. His Indian background plays a major role in his stand-up comedy.</p>
-                    <a href="https://ch.tudelft.nl/events/0c920374-07b4-4b31-bc70-f27580a9aeec" target="_blank" rel="noreferrer" className="Button">Tickets</a>
+                    {/* <a href="https://ch.tudelft.nl/events/0c920374-07b4-4b31-bc70-f27580a9aeec" target="_blank" rel="noreferrer" className="Button">Tickets</a> */}
                 </div>
             </div>
             <div data-num="18" className="Activity">
                 <h3>Week 1: Beer tasting</h3>
                 <p>Who has the best taste? On thursday we will gather at Het Proeflokaal (tasting room) of the Delfts brewery De Koperen Kat. In a timespan of 2 hours, several beers will be served. We’d love to see you there!</p>
-                <a href="https://ch.tudelft.nl/events/56a00f7f-e02a-44ef-9f98-9efc2546b34a" target="_blank" rel="noreferrer" className="Button">Tickets</a>
+                {/* <a href="https://ch.tudelft.nl/events/56a00f7f-e02a-44ef-9f98-9efc2546b34a" target="_blank" rel="noreferrer" className="Button">Tickets</a> */}
             </div>
             <div data-num="19" className="Activity">
                 <h3>Week 1: "Uitbrak" high tea</h3>
                 <p>As a closer for the first lustrum week we will be having a nice high tea in the /Pub during the lunch break. Come on down to have some delicious traditional scones along with many other different sweet or savory bites!</p>
-                <a href="https://ch.tudelft.nl/events/b5aaae15-6bde-4b63-ad55-5fbd832d9cb0" target="_blank" rel="noreferrer" className="Button">Tickets</a>
+                {/* <a href="https://ch.tudelft.nl/events/b5aaae15-6bde-4b63-ad55-5fbd832d9cb0" target="_blank" rel="noreferrer" className="Button">Tickets</a> */}
             </div>
             <div data-num="27" className="Activity">
                 <h3>CHess tournament</h3>
@@ -85,12 +85,12 @@ const Timeline = () => {
                 <ul>
                     <li>11:00 - Opening</li>
                     <li>11.30 - Round 1</li>
-                    <li>12.10 - Round 2</li>
-                    <li>12.50 - Round 3</li>
-                    <li>13.30 - Break</li>
-                    <li>14.10 - Round 4</li>
-                    <li>14.50 - Round 5</li>
-                    <li>16.00 - Prize Ceremony</li>
+                    <li>12.20 - Round 2</li>
+                    <li>13.10 - Round 3</li>
+                    <li>14.00 - Break</li>
+                    <li>14.30 - Round 4</li>
+                    <li>15.20 - Round 5</li>
+                    <li>16.30 - Prize Ceremony</li>
                 </ul>
                 <a href="https://ch.tudelft.nl/events/b93a59a0-029b-49bb-a7b5-16c25759f5f1" target="_blank" rel="noreferrer" className="Button">Tickets</a>
             </div>
