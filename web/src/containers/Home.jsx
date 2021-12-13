f816d8dc20435271ed500254ca53b32802a5bed6
Joep de Jong <j.dejong-16@student.tudelft.nl>
2021-12-13 14:12:24 +0100

    Add lustrum album, calendar and ongoing game

--- web/src/containers/Home.jsx
+++ web/src/containers/Home.jsx
@@ -33,6 +33,19 @@ const Home = () => {
                         <a href="mailto:lucie-extern@ch.tudelft.nl" target="_blank" rel="noreferrer" className="Button">Become a partner</a>
                     </div>
                 </section>
+                <section id="album">
+                    <Element name="album"></Element>
+                    <div className="Block">
+                        <h2>Lustrum Album</h2>
+                        <p>Did you know that you can contribute to the lustrum album? 🎶 You can write your own song or make a cover! Claim your song through the forms in bio to make sure you will be heard on the one and only lustrum album! The deadline to submit your song is 19 December.</p>
+
+                        <p>Finsihed songs can be submitted via e-mail (<a href="mailto:lucie@ch.tudelft.nl">lucie@ch.tudelft.nl</a>). If the size of the attachment is too large, we suggest to use WeTransfer.</p>
+                        <div className="Button-wrapper">
+                            <a href="https://docs.google.com/forms/d/1h3XdGjiokS3IC450lztm-enzHNTTB3Pd57jbqjSsrrg/edit" target="_blank" rel="noreferrer" className="Button" disabled>Claim your song</a>
+                            <a href="https://wetransfer.com/" target="_blank" rel="noreferrer" className="Button">WeTransfer</a>
+                        </div>
+                    </div>
+                </section>
                 <section id="merchandise">
                     <Element name="merchandise"></Element>
                     <div className="Block">
@@ -42,6 +55,32 @@ const Home = () => {
                         <p>It is possible to see the longsleeves in person before ordering them online. There are some samples in the association room of CH, which can also be used for fitting. The samples are subject to change slightly.</p>
                         <div className="Button-wrapper">
                             <a href="https://ch.tudelft.nl/events/76bf1bec-3530-468f-b84e-9447474e0fa9/" target="_blank" rel="noreferrer" className="Button Button--disabled" disabled>Sold out</a>
+                        </div>
+                    </div>
+                    <div className="Block">
+                        <h2>Calendar</h2>
+                        <p>Do you like to be part of our fabulous lustrum CHalendar? Then this is your time!</p>
+                        <p>If you are interested in shooting a photo with your friends/committee/board or by yourself, such that you guys will represent a month in our CHalendar, please fill in this form and we will reach out to you! </p>
+                        <div className="Button-wrapper">
+                            <a href="https://docs.google.com/forms/d/e/1FAIpQLSdSMaQVBzwFjibP832Nk9p0qh0NZpfkEGC4pIU73RPpzGwPLw/viewform?usp=sf_link" target="_blank" rel="noreferrer" className="Button">Fill in the form</a>
+                        </div>
+                    </div>
+                </section>
+                <section id="album">
+                    <div className="Block">
+                        <h2>Ongoing Game: CHristmas Puzzle</h2>
+                        <p>Every month, around the 13th, there is an ongoing game organised by the LuCie. For every month you participate you get at least one "ticket" for the lottery at the end of the lustrumyear. Participating multiple times results in a higher win chance. People who perform best in an ongoing game are awarded with some bonus "tickets", to increase their chances.</p>
+                        <p>This month we made a small CHristmas Puzzle, inspired by the yearly <a href="https://www.aivd.nl/onderwerpen/aivd-kerstpuzzel" target="_blank" rel="noreferrer">AIVD Kerstpuzzel</a>. Participating results in 1 ticket, answering the question "Who?" correctly gives you another ticket. Additional tickets may be rewarded for the fastest correct replies on the form and creativity.</p>
+                        <p>We kindly ask you to not share solutions with other participants.</p>
+
+                        <h3>The Puzzle:</h3>
+                        <textarea>
+                            0000101111100000000010100011101001010001010100101000111000000001010000000001111110000000010000000000000000100000111000010111010100001000001010000101110111000000000000000
+                        </textarea>
+                        <h3>The Question:</h3>
+                        <p>Who?</p>
+                        <div className="Button-wrapper">
+                            <a href="https://forms.gle/go4h3f1htGjSYyD98" target="_blank" rel="noreferrer" className="Button">Submit your answer</a>
                         </div>
                     </div>
                 </section>
